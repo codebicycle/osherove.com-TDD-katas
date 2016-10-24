09dbcfc8556971591085211f1da77d88a7f8ae2a
codebicycle <codebicycle@gmail.com>
2016-10-24 18:48:44 +0300

    Add first test

---
+++ test_string_calculator.py
@@ -0,0 +1,28 @@
+"""
+Run unittest tests: $ python3 -m unittest
+Run pytest tests:   $ pytest
+
+"""
+
+from string_calculator import StringCalculator
+
+# import unittest
+
+# class StringCalculatorTest(unittest.TestCase):
+
+#     def setUp(self):
+#         self.calculator = StringCalculator()
+
+#     def test_add_empty_string(self):
+#         result = self.calculator.add('')
+#         self.assertEqual(0, result)
+
+import pytest
+
+@pytest.fixture
+def calculator():
+    return StringCalculator()
+
+def test_add_empty_string(calculator):
+    result = calculator.add('')
+    assert 0 == result
